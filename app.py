eeecb06f70749379eff9b010a76733b0d9ecce51
showribabu <kantashowribabu.com>
2023-11-29 20:59:18 +0530

    MongoDb used

--- app.py
+++ app.py
@@ -2,15 +2,27 @@ from flask import Flask,render_template,redirect,request
 import pymysql as mysql
 
 
+from pymongo import MongoClient
+
+connection_string = 'mongodb+srv://kantashowribabu:showribabu@cluster1.es2isby.mongodb.net/'
+
+# Create the MongoDB client
+client = MongoClient(connection_string)
+
+# Access the 'attendance' database and 'admin' collection
+db = client['attendance']
+
+
+
 #obj..
 app=Flask(__name__)
-# mydb=mysql.connect(
-#     host='localhost',
-#     user='root',
-#     password='Ksb6419*',
-#     database='attendance'
-# )
-# cursor=mydb.cursor()
+mydb=mysql.connect(
+    host='localhost',
+    user='root',
+    password='Ksb6419*',
+    database='attendance'
+)
+cursor=mydb.cursor()
 
 
 #handlers...
@@ -46,6 +58,10 @@ def faculty_signup():
     return render_template('faculty-signup.html')
 
 
+#requests handling by admin....
+@app.route('/requestshandle')
+def  requestshandle():
+    return render_template('requests.html')
 
 #attendance view by admin
 
@@ -87,26 +103,62 @@ def studentadd():
 
 @app.route('/adlogin',methods=['POST'])
 def adlogin():
+    adcoll=db['admin']
+    
     name=request.form['username']
     password=request.form['password']
+    if(name=='' and password==''):
+        return render_template('admin-login.html',m='Please fill all details!!')
+    
     
     print(name,password)
-    sql= 'SELECT * FROM admin'
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    print(type(result))
     
-    for i in result:
-        print(i)
-        if i[0]==name and i[1]==password:
-            return render_template('admin-home.html')
-        else:
-            return render_template('admin-login.html',m='Please enter valid details')
+    # sql= 'SELECT * FROM admin'
+    # cursor.execute(sql)
+    # result = cursor.fetchall()
+    # print(type(result))
+    
+    # for i in result:
+    #     print(i)
+    #     if i[0]==name and i[1]==password:
+    #         return render_template('admin-home.html')
+    #     else:
+    #         return render_template('admin-login.html',m='Please enter valid details')
+    
+    d=adcoll.find()
+    
+
+    if (d[0]['username'] == name and d[0]['password'] == password):
+        return render_template('admin-home.html')
+    else:
+        return render_template('admin-login.html',m='Please enter valid details')
+        
+
+
+#faculty-signup
+
+@app.route('/faculty-signup',methods=['POST'])
+def facultysignup():
+'''
+fname
+lname
+username
+gender
+designation
+department
+password
+cpassword
+'''
+    pass    
+                     
+    
+    
     
     
 
 
 if __name__=='__main__':
+    
     app.run(debug=True)
     
 
