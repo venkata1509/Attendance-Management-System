44d8ec2b0df2206a7d68c2e5ea6a5bc3a6497db1
JagadeeshReddi <jagadeeshreddii4u@gmail.com>
2023-11-29 12:37:06 +0530

    Modified logins

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import pymysql as mysql
 
 #obj..
 app=Flask(__name__)
-mydb=mysql.connect(
-    host='localhost',
-    user='root',
-    password='Ksb6419*',
-    database='attendance'
-)
-cursor=mydb.cursor()
+# mydb=mysql.connect(
+#     host='localhost',
+#     user='root',
+#     password='Ksb6419*',
+#     database='attendance'
+# )
+# cursor=mydb.cursor()
 
 
 #handlers...
@@ -20,10 +20,21 @@ cursor=mydb.cursor()
 def start():
     return render_template('index.html')
 
+@app.route('/admin-login')
+def admin_login():
+    return render_template('admin-login.html')
 
+@app.route('/faculty-login')
+def faculty_login():
+    return render_template('faculty-login.html')
 #server start..
 
+@app.route('/student-login')
+def student_login():
+    return render_template('student-login.html')
+
+
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
     
 
